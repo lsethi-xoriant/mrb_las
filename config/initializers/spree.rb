ed6346b2a48e6584b5e8ff0148150cd297403233
lsethi-xoriant <lalit.rubyonrails@gmail.com>
2016-05-15 12:50:45 +0530

    config changes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,33 +17,32 @@ Spree.config do |config|
    # config.s3_bucket = 'expressmobileproducts'		
    # config.s3_access_key = "AKIAJLMAISGBIX4SDW3A"		
    # config.s3_secret = "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"
-   Spree.config do |config|
    #S3 configuration
-      if Rails.env.production? then
-           #production. Store images on S3.
-           # development will default to local storage
-          attachment_config = {
-          s3_credentials: {
-            access_key_id: 'AKIAJLMAISGBIX4SDW3A',
-            secret_access_key:'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' ,
-            bucket: 'expressmobileproducts',
-          },
+  	if Rails.env.production? then
+       #production. Store images on S3.
+       # development will default to local storage
+      attachment_config = {
+      s3_credentials: {
+        access_key_id: 'AKIAJLMAISGBIX4SDW3A',
+        secret_access_key:'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' ,
+        bucket: 'expressmobileproducts',
+      },
 
 
-          storage:        :s3,
-          s3_headers:     { "Cache-Control" => "max-age=31557600" },
-          s3_protocol:    "https",
-          bucket:        'expressmobileproducts',
+      storage:        :s3,
+      s3_headers:     { "Cache-Control" => "max-age=31557600" },
+      s3_protocol:    "https",
+      bucket:        'expressmobileproducts',
 
-          path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
-          default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-          default_style: "product",
-          }
+      path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
+      default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+      default_style: "product",
+      }
 
-          attachment_config.each do |key, value|
-               Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-          end
-     end
+      attachment_config.each do |key, value|
+           Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+      end
+ 	end
 end
 
 Spree.user_class = "Spree::LegacyUser"
